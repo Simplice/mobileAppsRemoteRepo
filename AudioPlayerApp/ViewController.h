bc0e98a97da6c90cd5d8a39d50f03ae909107d0c
Simplice Tchoupkoua <simplice.tchoupkoua@adorsys.de>
2013-07-08 10:11:08 +0200

    Add news music files

--- AudioPlayerApp/ViewController.h
+++ AudioPlayerApp/ViewController.h
@@ -7,7 +7,24 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h> // this librairy is important for all Apps with music player
 
-@interface ViewController : UIViewController
+/*
+ To use PickeView it is important and necessary to implement the protocole UIPickerViewDataSource and UIPickerViewDelegate
+ */
+@interface ViewController : UIViewController <UIPickerViewDataSource , UIPickerViewDelegate, AVAudioPlayerDelegate>
+// notre ViewController doit implémenter les protocoles entre lec crochets <>
+
+@property (nonatomic, strong) AVAudioPlayer *player;
+@property (strong, nonatomic) NSMutableArray *myPlayListe;
+@property (strong, nonatomic) NSTimer *timer;
+
+@property (strong, nonatomic) IBOutlet UIPickerView *pickerView;
+@property (strong, nonatomic) IBOutlet UILabel *dureeTotaleDuSong;
+@property (strong, nonatomic) IBOutlet UIProgressView *progressBar;
+
+- (IBAction)play:(id)sender;
+- (IBAction)stop:(id)sender;
+- (IBAction)volume:(id)sender;
 
 @end
--- AudioPlayerApp/ViewController.m
+++ AudioPlayerApp/ViewController.m
@@ -14,16 +14,101 @@
 
 @implementation ViewController
 
+@synthesize player, myPlayListe, dureeTotaleDuSong, timer;
+
 - (void)viewDidLoad
 {
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSDictionary *music1 = [[NSDictionary alloc] initWithObjectsAndKeys:@"myReggae", @"title", @"My own Song", @"interpreter", nil];
+    NSDictionary *music2 = [[NSDictionary alloc] initWithObjectsAndKeys:@"Alone", @"title", @"Claude Kelly", @"interpreter", nil];
+    NSDictionary *music3 = [[NSDictionary alloc] initWithObjectsAndKeys:@"Only one night", @"title", @"Claude Kelly", @"interpreter", nil];
+    NSDictionary *music4 = [[NSDictionary alloc] initWithObjectsAndKeys:@"Number One", @"title", @"Jamie Foxx", @"interpreter", nil];
+    NSDictionary *music5 = [[NSDictionary alloc] initWithObjectsAndKeys:@"Number One Fan", @"title", @"Plies", @"interpreter", nil];
+    NSDictionary *music6 = [[NSDictionary alloc] initWithObjectsAndKeys:@"thank You", @"title", @"jamelia et singuila", @"interpreter", nil];
+    
+    myPlayListe = [[NSMutableArray alloc] initWithObjects:music1, music2, music3, music4, music5, music6, nil];
+    
+    NSDictionary *initSong = [self.myPlayListe objectAtIndex:0];
+    [self registerSongToPlay:[initSong objectForKey:@"title"]];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void) registerSongToPlay: (NSString *) songTitle {
+    // 1. get the path of the mp3 file
+    NSString *path = [[NSBundle mainBundle] pathForResource:songTitle ofType:@"mp3"];
+    // 2. get the URL of the mp3 file
+    NSURL *url = [NSURL fileURLWithPath:path];
+    player = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:nil];
+    
+    // 3. show the duration of the song
+    long longueurDuSong = [self.player duration];
+    self.dureeTotaleDuSong.text = [NSString stringWithFormat:@"Durée totale: %i Secondes", (int)longueurDuSong];
+    // 4 ProgressView to show how long the song is gonna play
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(progressViewAktualisieren) userInfo:nil repeats:YES];
+}
+
+-(void) progressViewAktualisieren {
+    float durationTotal = self.player.duration;
+    float currentTime = self.player.currentTime;
+    [self.progressBar setProgress:currentTime / durationTotal];
+}
+
+#pragma mark - picker view methods
+// cette methode définit le nombre de colonnes dans le Pickerview
+-(NSInteger) numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
+}
+// cette methode définit le nombre d'éléments(lignes) affichés dans le Picker
+-(NSInteger) pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return [myPlayListe count];
+}
+// cette methode retourne les valeurs à afficher dans le pickerview. ces valeurs sont extraites de myArrayListe
+-(NSString *) pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    NSDictionary *currentObject = [myPlayListe objectAtIndex:row];
+    return [currentObject objectForKey:@"title"];
+}
+// cette methode actualise notre label avec la valeur selectionée dans le pickerview
+-(void) pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    NSDictionary *currentObject = [myPlayListe objectAtIndex:row];
+    NSString *selectedSong = [currentObject objectForKey:@"title"];
+    // if the player is in used, then stop it before starting playing the new selected song.
+    if ([self.player isPlaying]) {
+        [self.player stop];
+    }
+    [self registerSongToPlay:selectedSong];
+    [self.player play];
+}
+
+#pragma mark - AVFundation Music-Player methods
+- (IBAction)play:(id)sender {
+    NSLog(@"%s", __PRETTY_FUNCTION__);
+    [player play];
+}
+
+- (IBAction)stop:(id)sender {
+    NSLog(@"%s", __PRETTY_FUNCTION__);
+    [player stop];
+}
+
+- (IBAction)volume:(id)sender {
+    NSLog(@"%s", __PRETTY_FUNCTION__);
+    UISlider *slider = sender;
+    [player setVolume:slider.value];
+}
+
+/*****************************************************************************************************************************
+ Cette methode provient de AVAudioPlayerDelegate, pour cette raison il faut ajouter ce protocole dans le fichier .h
+ ****************************************************************************************************************************/
+- (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag {
+    [self.timer invalidate];
 }
 
 @end
